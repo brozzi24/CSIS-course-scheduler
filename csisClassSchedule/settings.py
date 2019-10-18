c240a9e2e59cc6a5f05db54391a71ca4c0b2f11b
brozzi24 <bmrozzi@student.ysu.edu>
2019-10-17 23:43:39 -0400

    Update settings.py

--- csisClassSchedule/settings.py
+++ csisClassSchedule/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'classes.apps.ClassesConfig',
     'rooms.apps.RoomsConfig',
     # 'scheduledClass.apps.ScheduledclassConfig',
+    'django.contrib.sites',
+     'account',
 
     
 
